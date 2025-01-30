1dcba317dac13ca1a3d481872c4d4bd4d466aafb
Arjun Acharya <acharayaarjun28@gmail.com>
2025-01-30 17:23:36 +0545

    first commit

--- newsnepal/settings.py
+++ newsnepal/settings.py
@@ -150,7 +150,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
     "https://news-frontend-rsa1.onrender.com",
 ]
 CSRF_TRUSTED_ORIGINS = [
